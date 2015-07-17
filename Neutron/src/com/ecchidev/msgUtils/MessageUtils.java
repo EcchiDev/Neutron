2fbdf0ef698e7f5a5a09a545fde073fefc1d581d
Jonne Saloranta <mastersteve26@gmail.com>
2015-07-18 02:56:39 +0300

    Now it's easier to use colors

---
+++ Neutron/src/com/ecchidev/msgUtils/MessageUtils.java
@@ -0,0 +1,35 @@
+package com.ecchidev.msgUtils;
+
+import org.bukkit.ChatColor;
+
+//Jonne Saloranta - www.youtube.com/MasterSteve26
+
+public class MessageUtils {
+	
+	//add 'extends MessageUtils' to make it easier to use colors...
+	
+	
+	public static ChatColor AQUA  = ChatColor.AQUA;
+	public static ChatColor BLACK  = ChatColor.BLACK;
+	public static ChatColor BLUE  = ChatColor.BLUE;
+	public static ChatColor BOLD  = ChatColor.BOLD;
+	public static ChatColor DARK_AQUA  = ChatColor.DARK_AQUA;
+	public static ChatColor DARK_BLUE  = ChatColor.DARK_BLUE;
+	public static ChatColor DARK_GRAY  = ChatColor.DARK_GRAY;
+	public static ChatColor DARK_GREEN  = ChatColor.DARK_GREEN;
+	public static ChatColor DARK_PURPLE  = ChatColor.DARK_PURPLE;
+	public static ChatColor DARK_RED  = ChatColor.DARK_RED;
+	public static ChatColor GOLD  = ChatColor.GOLD;
+	public static ChatColor GRAY  = ChatColor.GRAY;
+	public static ChatColor GREEN  = ChatColor.GREEN;
+	public static ChatColor ITALIC  = ChatColor.ITALIC;
+	public static ChatColor LIGHT_PURPLE  = ChatColor.LIGHT_PURPLE;
+	public static ChatColor MAGIC  = ChatColor.MAGIC;
+	public static ChatColor RED  = ChatColor.RED;
+	public static ChatColor RESET  = ChatColor.RESET;
+	public static ChatColor STRIKETHROUGH  = ChatColor.STRIKETHROUGH;
+	public static ChatColor UNDERLINE  = ChatColor.UNDERLINE;
+	public static ChatColor WHITE  = ChatColor.WHITE;
+	public static ChatColor YELLOW  = ChatColor.YELLOW;
+
+}
